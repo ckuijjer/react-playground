4e0c34c54a31cde3b83d86102d726da85b59bbbb
Casper Kuijjer <casper@kuijjer.com>
2018-07-06 11:35:47 +0200

    redux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Provider } from 'react-redux'
+
+import store from './store'
 import LifeCycle from './LifeCycle'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Provider store={store}>
         <LifeCycle />
-      </div>
+      </Provider>
     )
   }
 }
